1768beaf36d3a2f5ce5dc4ff082e19a94dba813c
Brooks Poltl <bpoltl1@gmail.com>
2021-03-27 14:32:35 -0500

    hello world

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,6 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
+  # implicit return, auto returns last thing
   def hello do
-    :world
+    "hi there!"
   end
 end
