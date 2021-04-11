bca758efa0ab3ffc298c6f80c92ffc9c0cd07759
Brooks Poltl <bpoltl1@gmail.com>
2021-04-10 21:03:27 -0500

    add association

--- discuss/lib/discuss/accounts/user.ex
+++ discuss/lib/discuss/accounts/user.ex
@@ -6,7 +6,7 @@ defmodule Discuss.Accounts.User do
     field :email, :string
     field :token, :string
     field :provider, :string
-
+    has_many :topics, Discuss.Discussions.Topic
     timestamps()
   end
 
